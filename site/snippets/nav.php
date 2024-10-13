8b0927d74bdc06ae3f10013917fb7d6f23c26617
goeht <tpgoedert@outlook.com>
2024-10-13 20:37:21 +0200

    fix lang switcher

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,21 +1,34 @@
 <nav class="mob-hide nav-grid desk-nav">
   <ul>
     <li class="dropdown">
-      <a href="#"><?= $site->page("series")->title()?></a>
+      <a href="#"><?= $site->page("series")->title() ?></a>
       <div class="dropdown-content dark-drop">
-        <?php foreach($site->page('series')->children() as $p): ?>
-          <?php if( $kirby->language() == 'fr'):?>
+        <?php foreach ($site->page("series")->children() as $p): ?>
+          <?php if ($kirby->language() == "fr"): ?>
             <a href="<?= $p->url() ?>"><?= $p->title() ?></a>
-          <?php else :?>
-            <a href="<?= $p->url('en') ?>"><?= $p->title() ?></a>
-          <?php endif ?>
-        <?php endforeach ?>
+          <?php else: ?>
+            <a href="<?= $p->url("en") ?>"><?= $p->title() ?></a>
+          <?php endif; ?>
+        <?php endforeach; ?>
       </div>
     </li>
   </ul>
   <span class="nav-center"></span>
   <ul class="flex" style="justify-self: baseline;">
-    <li><a href="<?= $site->page("about")->url()?>"><?= $site->page("about")->title()?></a></li>
-    <li><a href="<?= $site->url() ?>?lang=en">EN</a> | <a href="<?= $site->url() ?>?lang=fr"> FR </a></li>
+    <li><a href="<?= $site->page("about")->url() ?>"><?= $site
+    ->page("about")
+    ->title() ?></a></li>
+
+    <li>
+        <?php foreach ($kirby->languages() as $language): ?>
+        <a <?php e(
+            $kirby->language() == $language,
+            ' class="active"'
+        ); ?> href="<?php echo $language->url(); ?>" hreflang="<?php echo $language->code(); ?>" data-no-swup>
+          <?php echo html($language->code()); ?>
+        </a>
+        <?php endforeach; ?>
+    </li>
+
   </ul>
 </nav>
